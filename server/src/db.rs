27a182978df7fd6ececaaac4d16fd8adc258aaff
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-07-01 17:05:51 -0400

    get id

--- server/src/db.rs
+++ server/src/db.rs
@@ -1,9 +1,12 @@
 use mongodb::db::ThreadedDatabase;
+use mongodb::oid::ObjectId;
 use mongodb::{Bson, Client, ThreadedClient};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TrafficData {
+    #[serde(rename = "_id")]
+    pub id: ObjectId,
     pub lhrs: i32,
     pub hwy_number: i32,
     pub hwy_type: String,
