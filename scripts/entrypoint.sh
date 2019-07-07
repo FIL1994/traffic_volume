1292e0532d300d43056f1fb0e25e9ac66e0819be
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-07-06 21:19:21 -0400

    updated entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
-echo "starting"
-
 # wait for mongodb to start 
 ./scripts/wait-for-it.sh mongo:27017 -t 30
 
 if test ! -f "/did_insert.txt"
 then
-    echo "inserting...""
+    echo "inserting records..."
     cd /usr/local/app/traffic_records
     cargo run
     touch /did_insert.txt
