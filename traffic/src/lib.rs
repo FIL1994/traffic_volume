515b2d1345918cfd71e9e484961b902b2ba8dc58
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-07-09 20:02:32 -0400

    added comments for abbreviations

--- traffic/src/lib.rs
+++ traffic/src/lib.rs
@@ -4,13 +4,18 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Copy, Clone, Debug, GraphQLObject)]
 pub struct TrafficYearData {
     pub year: i32,
+    /** Design Hour Volume */
     pub dhv: f64,
     pub directional_split: f64,
+    /** Annual Average Daily Traffic */
     pub aadt: i32,
     pub aadt_yearly_change: f64,
     pub aadt_10_year_change: Option<f64>,
+    /** Summer Average Daily Traffic */
     pub sadt: i32,
+    /** Summer Average Weekday Traffic */
     pub sawdt: i32,
+    /** Winter Average Daily Traffic */
     pub wadt: i32,
 }
 
@@ -22,13 +27,16 @@ pub struct TravelPattern {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TrafficData {
+    /** Linear Highway Referencing System */
     pub lhrs: i32,
     pub hwy_number: i32,
     pub hwy_type: String,
     pub location_desc: String,
+    /** Regional */
     pub reg: String,
     pub section_length: f64,
     pub connecting_link_length: f64,
+    /** (for Connecting Links, Regional Boundarys,etc) */
     pub secondary_desc: String,
     pub travel_patterns: Vec<TravelPattern>,
 }
